81c5afcb58236c8609880ca3a2d45fd58a82357c
Evelyn Cassinotte <e.cass03@gmail.com>
2024-09-03 09:14:57 -0300

    update: aula 09-02

---
+++ 09-02/media/assets/scripts/script.js
@@ -0,0 +1,5 @@
+/*Capturar dados do formulário*/
+// Declarar as váriaveis para receber os dados do formulário
+// Declarar constantes para pesos
+// Calcular a média final
+// Exibir a média final e resultado Aprovado(>=6) ou Reprovado(<6)
