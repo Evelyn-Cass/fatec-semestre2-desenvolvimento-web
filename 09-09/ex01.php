05540139ef00dffd9b1e05263da8c94711a0288b
Evelyn Cassinotte <e.cass03@gmail.com>
2024-09-09 09:14:38 -0300

    add: aula 09-09

---
+++ 09-09/ex01.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+<h1>Exercício 1 - Processando POST na mesma página PHP.</h1>
+<form action="#" method="POST">
+    <input type="text" name="num1" placeholder="Número 1">
+    <input type="text" name="num2" placeholder="Número 2">
+    <input type="submit" value="Enviar" name="enviar">
+    <input type="reset" name="limpar" value="Limpar">
+</form>
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $n1 = $_POST["num1"];
+        $n2 = $_POST["num2"];
+        $soma =  $n1 + $n2;
+        echo "<hr><h2>Resultado do exercício 1</h2><br>";
+        echo "A soma de $n1 + $n2 é $soma";
+    }
+?>
+<br>
+<hr>
+
+<a href="ex01.php" class="button">Voltar</a>
